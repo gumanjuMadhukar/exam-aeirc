6249bdb9920d64b1337b60cc120dbc598efc956e
Abhishek Thapa <abhishek.thapa@diagonal.software>
2023-04-11 10:06:18 +0545

    Quiz screen for students

--- pages/dir/quiz/index.tsx
+++ pages/dir/quiz/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import QuestionAPI from "apis/question";
-import { Checkbox, Radio } from "antd";
+import { Button, Checkbox, Col, Divider, Radio, Row, Space, Tag } from "antd";
+import styled from "styled-components";
 
 const Quiz = () => {
   const [questions, setQuestions] = useState<any>([]); // Store the fetched questions
@@ -15,6 +16,25 @@ const Quiz = () => {
     return response?.data?.data;
   });
 
+  const FullScreenPage = () => {
+  useEffect(() => {
+    const enterFullScreen = () => {
+      const elem = document.documentElement;
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } 
+    };
+
+    enterFullScreen();
+
+    // Clean up the full-screen mode when the component unmounts
+    return () => {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    };
+  }, []);
+
   useEffect(() => {
     setQuestions(queryList?.data);
   }, [queryList]);
@@ -23,39 +43,151 @@ const Quiz = () => {
     // Store the answer in the answers object
     setAnswers((prevAnswers: any) => ({
       ...prevAnswers,
-      [currentQuestion]: answer,
+      answer,
     }));
 
+    console.log(answers);
     // Move to the next question
+  };
+
+  const handleButtonClick = () => {
+    // Perform additional logic or submit answers
+    console.log("Answers:", answers);
+
+    console.log(questions[currentQuestion]);
     setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+    setAnswers({});
   };
 
   if (questions?.length === 0 || currentQuestion >= questions?.length) {
-    return <h1>Thank you!</h1>;
+    return (
+      <QuizFinished>
+        <h1>Thank you! All the best for the result</h1>;
+      </QuizFinished>
+    );
   } else {
     if (questions) {
       const question = questions[currentQuestion];
       return (
-        <div>
+        <>
           {" "}
-          <h2>{question?.question_text}</h2>
-          <ul>
-            {question?.question_type === "checkbox"
-              ? question?.options.map((option: any, index: any) => (
-                  <Checkbox key={index}>{option?.option_text}</Checkbox>
-                ))
-              : question?.question_type === "radio" && (
-                  <Radio.Group>
+          <Space size={[0, 8]} wrap>
+            <StyleTag color="blue">
+              Question {currentQuestion + 1} of {questions?.length}
+            </StyleTag>
+          </Space>
+          <Divider orientation="left"></Divider>
+          <QuizContainer>
+            <Row style={{ fontSize: "24px", fontWeight: "700" }}>
+              {currentQuestion + 1}. {"  "} {question?.question_text}
+            </Row>
+            <br />
+            <Row>
+              {question?.question_type === "checkbox" ? (
+                <Checkbox.Group
+                  onChange={(checkedValues) =>
+                    handleAnswerSubmit(checkedValues)
+                  }
+                >
+                  {question?.options.map((option: any, index: any) => (
+                    <OptionText>
+                      <Row>
+                        <Col lg={24} xs={24} md={24}>
+                          <Checkbox
+                            key={index}
+                            value={option?.id}
+                            style={{ fontSize: "18px" }}
+                          >
+                            {option?.option_text}
+                          </Checkbox>
+                        </Col>
+                      </Row>
+                      <br></br>
+                    </OptionText>
+                  ))}
+                </Checkbox.Group>
+              ) : (
+                question?.question_type === "radio" && (
+                  <Radio.Group
+                    onChange={(e) => handleAnswerSubmit(e.target.value)}
+                    style={{ paddingLeft: "18px" }}
+                  >
                     {question?.options.map((option: any, index: any) => (
-                      <Radio value={option?.id}>{option?.option_text}</Radio>
+                      <OptionText>
+                        <Radio value={option?.id} style={{ fontSize: "18px" }}>
+                          {option?.option_text}
+                        </Radio>
+                        <br />
+                      </OptionText>
                     ))}
                   </Radio.Group>
-                )}
-          </ul>
-        </div>
+                )
+              )}
+            </Row>
+            <CustomizedButtonGroup>
+              <Button
+                style={{ marginTop: "20px" }}
+                type="primary"
+                size="large"
+                onClick={handleButtonClick}
+              >
+                Next
+              </Button>
+            </CustomizedButtonGroup>
+          </QuizContainer>
+        </>
       );
     }
   }
 };
 
 export default Quiz;
+
+const QuizContainer = styled.div`
+  // background: white;
+  padding: 10px;
+  width: 60%;
+  margin: auto;
+  // display: flex;
+  // justify-content: center;
+  // flex-direction: column;
+  // align-items: center;
+  // height: 100vh;
+  // width: 100vw;
+`;
+
+const StyleTag = styled(Tag)`
+  color: white;
+  background-color: #ff7000;
+  border: none;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 5px;
+`;
+
+const QuizFinished = styled.div`
+  background: white;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  height: 100vh;
+  fontweight: 700;
+  font-size: 14px;
+  // width: 100vw;
+`;
+
+const OptionText = styled.div`
+  // font-size: 22px;
+  font-weight: 500;
+  margin-top: 10px;
+  // width: 100vw;
+`;
+
+const CustomizedButtonGroup = styled.div`
+  float: right;
+  margin-top: 10px;
+
+  margin-right: 50px;
+`;
